53855125741a8b4ae4027b583fb95695e38e4929
Josef Hoerandtner <josef.hoerandtner@springer.com>
2021-05-06 22:32:48 +0200

    Use pip for correct python version (3.5)

--- download_dependencies.sh
+++ download_dependencies.sh
@@ -7,7 +7,7 @@ wget https://pkgconfig.freedesktop.org/releases/pkg-config-0.29.tar.gz
 wget https://ftp.pcre.org/pub/pcre/pcre-8.43.tar.gz
 wget https://ftp.gnu.org/pub/gnu/ncurses/ncurses-6.1.tar.gz
 wget http://thrysoee.dk/editline/libedit-20190324-3.1.tar.gz
-wget https://bootstrap.pypa.io/get-pip.py -O get-pip-21.1.1.py
+wget https://bootstrap.pypa.io/pip/3.5/get-pip.py -O get-pip-21.1.1.py
 wget https://github.com/jemalloc/jemalloc/releases/download/5.2.0/jemalloc-5.2.0.tar.bz2
 wget https://varnish-cache.org/_downloads/varnish-6.0.7.tgz
 wget https://github.com/varnish/varnish-modules/archive/0.15.1.tar.gz -O varnish-modules-0.15.1.tar.gz
