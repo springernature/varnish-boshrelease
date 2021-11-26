c7165b132c03b9a25a997cfa5506387db49a378d
Josef Hoerandtner <josef.hoerandtner@springer.com>
2021-11-26 23:30:17 +0100

    Upgrade automake and pkg-config to make it compile on Bionic

--- add_blobs.sh
+++ add_blobs.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
 bosh add-blob autoconf-2.69.tar.gz autoconf/autoconf-2.69.tar.gz
-bosh add-blob automake-1.15.tar.gz autoconf/automake-1.15.tar.gz
+bosh add-blob automake-1.15.1.tar.gz automake/automake-1.15.1.tar.gz
 bosh add-blob libtool-2.4.6.tar.gz libtool/libtool-2.4.6.tar.gz
-bosh add-blob pkg-config-0.29.tar.gz pkg-config/pkg-config-0.29.tar.gz
+bosh add-blob pkg-config-0.29.2.tar.gz pkg-config/pkg-config-0.29.2.tar.gz
 bosh add-blob ncurses-6.1.tar.gz ncurses/ncurses-6.1.tar.gz
 bosh add-blob libedit-20190324-3.1.tar.gz libedit/libedit-20190324-3.1.tar.gz
+bosh add-blob ncurses-6.1.tar.gz ncurses/ncurses-6.1.tar.gz
 bosh add-blob get-pip-21.1.1.py varnish/get-pip-21.1.1.py
+bosh add-blob jemalloc-5.2.0.tar.bz2 varnish/jemalloc-5.2.0.tar.bz2
 bosh add-blob pcre-8.43.tar.gz varnish/pcre-8.43.tar.gz
 bosh add-blob varnish-6.0.9.tgz varnish/varnish-6.0.9.tgz
 bosh add-blob varnish-modules-0.15.1.tar.gz varnish/varnish-modules-0.15.1.tar.gz
--- download_dependencies.sh
+++ download_dependencies.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 wget https://ftp.gnu.org/gnu/autoconf/autoconf-2.69.tar.gz
-wget https://ftp.gnu.org/gnu/automake/automake-1.15.tar.gz
+wget https://ftp.gnu.org/gnu/automake/automake-1.15.1.tar.gz
 wget https://ftp.gnu.org/gnu/libtool/libtool-2.4.6.tar.gz
-wget https://pkgconfig.freedesktop.org/releases/pkg-config-0.29.tar.gz
-wget https://ftp.pcre.org/pub/pcre/pcre-8.43.tar.gz
+wget https://pkgconfig.freedesktop.org/releases/pkg-config-0.29.2.tar.gz
+wget https://ftp.exim.org/pub/pcre/pcre-8.43.tar.gz
 wget https://ftp.gnu.org/pub/gnu/ncurses/ncurses-6.1.tar.gz
 wget http://thrysoee.dk/editline/libedit-20190324-3.1.tar.gz
 wget https://bootstrap.pypa.io/pip/3.5/get-pip.py -O get-pip-21.1.1.py
