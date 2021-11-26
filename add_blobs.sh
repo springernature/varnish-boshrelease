c8849cd12fc9662a7e252c0e58b69aefc82d392f
Josef Hoerandtner <josef.hoerandtner@springer.com>
2021-11-26 23:21:54 +0100

    Revert "Update pcre"
    This reverts commit ed600c7530d2cdbf5ef8dcd26a815b38e84f0102.

--- add_blobs.sh
+++ add_blobs.sh
@@ -7,6 +7,6 @@ bosh add-blob pkg-config-0.29.2.tar.gz pkg-config/pkg-config-0.29.2.tar.gz
 bosh add-blob ncurses-6.1.tar.gz ncurses/ncurses-6.1.tar.gz
 bosh add-blob libedit-20190324-3.1.tar.gz libedit/libedit-20190324-3.1.tar.gz
 bosh add-blob get-pip-21.1.1.py varnish/get-pip-21.1.1.py
-bosh add-blob pcre-8.45.tar.gz varnish/pcre-8.45.tar.gz
+bosh add-blob pcre-8.43.tar.gz varnish/pcre-8.43.tar.gz
 bosh add-blob varnish-6.0.9.tgz varnish/varnish-6.0.9.tgz
 bosh add-blob varnish-modules-0.15.1.tar.gz varnish/varnish-modules-0.15.1.tar.gz
