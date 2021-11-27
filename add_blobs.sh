aa135c5032b6f310f719101836a41e5936e21b65
Josef Hoerandtner <josef.hoerandtner@springer.com>
2021-11-27 20:52:47 +0100

    Update ncurses to latest version

--- add_blobs.sh
+++ add_blobs.sh
@@ -4,7 +4,7 @@ bosh add-blob autoconf-2.69.tar.gz autoconf/autoconf-2.69.tar.gz
 bosh add-blob automake-1.15.1.tar.gz automake/automake-1.15.1.tar.gz
 bosh add-blob libtool-2.4.6.tar.gz libtool/libtool-2.4.6.tar.gz
 bosh add-blob pkg-config-0.29.2.tar.gz pkg-config/pkg-config-0.29.2.tar.gz
-bosh add-blob ncurses-6.1.tar.gz ncurses/ncurses-6.1.tar.gz
+bosh add-blob ncurses-6.3.tar.gz ncurses/ncurses-6.3.tar.gz
 bosh add-blob libedit-20190324-3.1.tar.gz libedit/libedit-20190324-3.1.tar.gz
 bosh add-blob ncurses-6.1.tar.gz ncurses/ncurses-6.1.tar.gz
 bosh add-blob get-pip-21.1.1.py varnish/get-pip-21.1.1.py
