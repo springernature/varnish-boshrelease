41042bf818bc8316e84e30ed4fa6a6aee07081e8
Josef Hoerandtner <josef.hoerandtner@springer.com>
2023-06-05 14:41:47 +0200

    Upgrade jemalloc and autoconf

--- scripts/add_blobs.sh
+++ scripts/add_blobs.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-bosh add-blob autoconf-2.69.tar.gz autoconf/autoconf-2.69.tar.gz
+bosh add-blob autoconf-2.71.tar.gz autoconf/autoconf-2.71.tar.gz
 bosh add-blob automake-1.15.1.tar.gz automake/automake-1.15.1.tar.gz
 bosh add-blob libtool-2.4.6.tar.gz libtool/libtool-2.4.6.tar.gz
 bosh add-blob pkg-config-0.29.2.tar.gz pkg-config/pkg-config-0.29.2.tar.gz
 bosh add-blob ncurses-6.3.tar.gz ncurses/ncurses-6.3.tar.gz
 bosh add-blob libedit-20210910-3.1.tar.gz libedit/libedit-20210910-3.1.tar.gz
 bosh add-blob get-pip-23.1.2.py varnish/get-pip-23.1.2.py
-bosh add-blob jemalloc-5.2.1.tar.bz2 varnish/jemalloc-5.2.1.tar.bz2
+bosh add-blob jemalloc-5.3.0.tar.bz2 varnish/jemalloc-5.3.0.tar.bz2
 bosh add-blob pcre-8.45.tar.gz varnish/pcre-8.45.tar.gz
 bosh add-blob varnish-6.0.11.tgz varnish/varnish-6.0.11.tgz
 bosh add-blob varnish-modules-0.15.1.tar.gz varnish/varnish-modules-0.15.1.tar.gz
--- scripts/download_dependencies.sh
+++ scripts/download_dependencies.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-wget https://ftp.gnu.org/gnu/autoconf/autoconf-2.69.tar.gz
+wget https://ftp.gnu.org/gnu/autoconf/autoconf-2.71.tar.gz
 wget https://ftp.gnu.org/gnu/automake/automake-1.15.1.tar.gz
 wget https://ftp.gnu.org/gnu/libtool/libtool-2.4.6.tar.gz
 wget https://pkgconfig.freedesktop.org/releases/pkg-config-0.29.2.tar.gz
@@ -8,6 +8,6 @@ wget https://ftp.exim.org/pub/pcre/pcre-8.45.tar.gz
 wget https://ftp.gnu.org/pub/gnu/ncurses/ncurses-6.3.tar.gz
 wget https://thrysoee.dk/editline/libedit-20210910-3.1.tar.gz
 wget https://bootstrap.pypa.io/get-pip.py -O get-pip-23.1.2.py
-wget https://github.com/jemalloc/jemalloc/releases/download/5.2.1/jemalloc-5.2.1.tar.bz2
+wget https://github.com/jemalloc/jemalloc/releases/download/5.3.0/jemalloc-5.3.0.tar.bz2
 wget https://varnish-cache.org/downloads/varnish-6.0.11.tgz
 wget https://github.com/varnish/varnish-modules/archive/0.15.1.tar.gz -O varnish-modules-0.15.1.tar.gz
